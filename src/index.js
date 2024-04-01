5398cdfc36fbb525faba2718f6ed5a4084b880d4
Iskelo <iskelo.kap@gmail.com>
2024-04-01 16:29:48 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import {fetchBreeds, fetchCatByBreed} from "./cat-api";
 
+const elements = {
+	select: document.querySelector('.breed-select'),
+	loader: document.querySelector('.loader'),
+	error: document.querySelector('.error'),
+	catInfo: document.querySelector('.cat-info')
+};
+
+const {select, loader, error, catInfo} = elements;
